955c8512c33d00734cf395b623a4a43af3dbd335
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-09 20:48:19 -0300

    feat: add register of AuthInterfase to injection

--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:todo_app/shared/data/sources/external/aut_source.dart';
 import 'package:todo_app/shared/implementations/dio_implementation.dart';
+import 'package:todo_app/shared/interfases/auth.dart';
 import 'package:todo_app/shared/interfases/http_client.dart';
 
 final injection = GetIt.instance;
@@ -8,4 +10,5 @@ void initDependencyInjection() {
   injection.registerLazySingleton<HttpClientInterfase>(
     () => HttpClientDioImplementation(),
   );
+  injection.registerFactory<AuthInterfase>(() => AuthSource());
 }
