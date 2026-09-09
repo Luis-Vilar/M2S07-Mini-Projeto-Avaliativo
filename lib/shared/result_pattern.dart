e469f5cc4f8ecc289b9e0cd5eb6ee0c0a0157094
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-09 15:19:24 -0300

    feature : add Result pattern

---
+++ lib/shared/result_pattern.dart
@@ -0,0 +1,25 @@
+sealed class Result<T> {
+  const Result();
+
+  /// Creates an instance of Result containing a value
+  factory Result.ok(T value) => Ok(value);
+
+  /// Create an instance of Result containing an error
+  factory Result.error(Exception error) => ResultError(error);
+}
+
+/// Subclass of Result for values
+final class Ok<T> extends Result<T> {
+  const Ok(this.value);
+
+  /// Returned value in result
+  final T value;
+}
+
+/// Subclass of Result for errors
+final class ResultError<T> extends Result<T> {
+  const ResultError(this.error);
+
+  /// Returned error in result
+  final Exception error;
+}
