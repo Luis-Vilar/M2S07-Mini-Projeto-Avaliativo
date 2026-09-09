6f0fe5443bba28843e4907954d448a46b3221ba7
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-09 19:25:11 -0300

    feat: implement AuthSource and AuthInterfase for user authentication

---
+++ lib/shared/data/sources/external/aut_source.dart
@@ -0,0 +1,23 @@
+import 'package:todo_app/core/injection.dart';
+import 'package:todo_app/shared/data/models/user/user_model.dart';
+import 'package:todo_app/shared/interfases/auth.dart';
+import 'package:todo_app/shared/interfases/http_client.dart';
+import 'package:todo_app/shared/result_pattern.dart';
+
+class AuthSource implements AuthInterfase {
+  final _httpClient = injection.get<HttpClientInterfase>();
+  @override
+  Future<Result> login(UserLoginModel user) async {
+    try {
+      final data = await _httpClient.post(
+        '/auth/login',
+        body: {'username': user.username, 'password': user.password},
+      );
+      return Result.ok(data);
+    } catch (e) {
+      return Result.error(
+        Exception('Não foi possível fazer login. Error : ${e.toString()}'),
+      );
+    }
+  }
+}
