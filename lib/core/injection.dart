8f4fb66f5c226704333fcbd7a761df0b4e5e3dd5
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-10 12:31:22 -0300

    feat: add TodoRepository registration in dependency injection

--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:todo_app/shared/data/sources/external/auth_source.dart';
 import 'package:todo_app/shared/implementations/dio_implementation.dart';
+import 'package:todo_app/shared/implementations/todo_repository_sqflite_implementation.dart';
 import 'package:todo_app/shared/interfaces/auth.dart';
 import 'package:todo_app/shared/interfaces/http_client.dart';
+import 'package:todo_app/shared/interfaces/todo_repository.dart';
 
 final injection = GetIt.instance;
 
@@ -11,4 +13,7 @@ void initDependencyInjection() {
     () => HttpClientDioImplementation(),
   );
   injection.registerFactory<AuthInterface>(() => AuthSource());
+  injection.registerLazySingleton<TodoRepositoryInterface>(
+    () => SqfliteTodoRepository(),
+  );
 }
