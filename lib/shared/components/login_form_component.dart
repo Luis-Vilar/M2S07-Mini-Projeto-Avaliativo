bb70fe8f2deffe96fd9230a266147c525839ecd7
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-10 02:19:58 -0300

    feat: add formKey to LoginFormComponent and pass it to LoginButtonComponent

--- lib/shared/components/login_form_component.dart
+++ lib/shared/components/login_form_component.dart
@@ -6,10 +6,12 @@ import 'package:todo_app/shared/components/login_button_component.dart';
 class LoginFormComponent extends StatelessWidget {
   const new({
     super.key,
+    required this.formKey,
     required this.userController,
     required this.passwordController,
   });
 
+  final GlobalKey<FormState> formKey;
   final TextEditingController userController;
   final TextEditingController passwordController;
 
@@ -25,42 +27,49 @@ class LoginFormComponent extends StatelessWidget {
               child: Center(
                 child: ConstrainedBox(
                   constraints: const BoxConstraints(maxWidth: 500),
-                  child: Column(
-                    crossAxisAlignment: .stretch,
-                    children: [
-                      const SizedBox(height: 60),
-                      const Center(
-                        child: Icon(
-                          Icons.person,
-                          size: 120,
-                          color: Colors.blue,
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      crossAxisAlignment: .stretch,
+                      children: [
+                        const SizedBox(height: 60),
+                        const Center(
+                          child: Icon(
+                            Icons.person,
+                            size: 120,
+                            color: Colors.blue,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 30),
-                      const Text(
-                        'Bem-vindo de volta!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 28, fontWeight: .w600),
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        'Organize suas tarefas e conquiste o seu dia.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 12, color: Colors.blueGrey),
-                      ),
-                      const SizedBox(height: 40),
-                      InputUserComponent(userController: userController),
-                      const SizedBox(height: 20),
-                      InputPasswordComponent(
-                        passwordController: passwordController,
-                      ),
-                      const SizedBox(height: 35),
-                      LoginButtonComponent(
-                        userController: userController,
-                        passwordController: passwordController,
-                      ),
-                      const SizedBox(height: 20),
-                    ],
+                        const SizedBox(height: 30),
+                        const Text(
+                          'Bem-vindo de volta!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 28, fontWeight: .w600),
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          'Organize suas tarefas e conquiste o seu dia.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        const SizedBox(height: 40),
+                        InputUserComponent(userController: userController),
+                        const SizedBox(height: 20),
+                        InputPasswordComponent(
+                          passwordController: passwordController,
+                        ),
+                        const SizedBox(height: 35),
+                        LoginButtonComponent(
+                          formKey: formKey,
+                          userController: userController,
+                          passwordController: passwordController,
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
                   ),
                 ),
               ),
