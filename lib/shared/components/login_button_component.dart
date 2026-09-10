eaf0402621240c38396471207bf16b2ba2833134
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-09 22:06:36 -0300

    feat: refactor LoginButtonComponent to use Bloc for login functionality

--- lib/shared/components/login_button_component.dart
+++ lib/shared/components/login_button_component.dart
@@ -1,13 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:todo_app/core/injection.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/shared/data/models/user/user_model.dart';
-import 'package:todo_app/shared/interfases/auth.dart';
-import 'package:todo_app/shared/result_pattern.dart';
+import 'package:todo_app/view_models/bloc/login_bloc.dart';
 
-class LoginButtonComponent extends StatelessWidget {
-  LoginButtonComponent({
+class LoginButtonComponent extends StatefulWidget {
+  const LoginButtonComponent({
     super.key,
     required this.userController,
     required this.passwordController,
@@ -15,46 +12,51 @@ class LoginButtonComponent extends StatelessWidget {
 
   final TextEditingController userController;
   final TextEditingController passwordController;
-  final auth = injection.get<AuthInterfase>();
 
   @override
+  State<LoginButtonComponent> createState() => _LoginButtonComponentState();
+}
+
+class _LoginButtonComponentState extends State<LoginButtonComponent> {
+  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 55,
-      child: ElevatedButton(
-        onPressed: () async {
-          final user = UserLoginModel(
-            username: userController.text,
-            password: passwordController.text,
-          );
-          final loginResult = await auth.login(user);
-
-          if (loginResult case Ok(:final value)) {
-            log(value.toString());
-          } else if (loginResult case ResultError(:final error)) {
-            log(error.toString());
-          }
-
-          userController.clear();
-          passwordController.clear();
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blueAccent,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
+    return BlocConsumer<LoginBloc, LoginState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return SizedBox(
+          width: double.infinity,
+          height: 55,
+          child: ElevatedButton(
+            onPressed: () async {
+              context.read<LoginBloc>().add(
+                LoginUserEvent(
+                  user: UserLoginModel(
+                    username: widget.userController.text,
+                    password: widget.passwordController.text,
+                  ),
+                ),
+              );
+              widget.userController.clear();
+              widget.passwordController.clear();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              elevation: 2,
+            ),
+            child: const Text(
+              'Fazer Login',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
           ),
-          elevation: 2,
-        ),
-        child: const Text(
-          'Fazer Login',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
