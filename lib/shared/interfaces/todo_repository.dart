1b44fbab8361784464a704f0fbb2c1652ef22c6a
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-10 12:30:57 -0300

    feat: add  TodoRepositoryInterface

---
+++ lib/shared/interfaces/todo_repository.dart
@@ -0,0 +1,8 @@
+import 'package:todo_app/shared/data/models/todo/todo_model.dart';
+
+abstract class TodoRepositoryInterface {
+  Future<void> insertTodo(TodoModel todo);
+  Future<List<TodoModel>> getTodos();
+  Future<void> updateTodo(TodoModel todo);
+  Future<void> deleteTodo(int id);
+}
