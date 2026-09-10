bb39dbe61a448249091590eccd1dcc3e313e1873
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-09 22:10:43 -0300

    refactor : moving main_app to core

--- lib/core/bootstrap.dart
+++ lib/core/bootstrap.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:todo_app/core/injection.dart';
-import 'package:todo_app/main_app.dart';
+import 'package:todo_app/core/main_app.dart';
 
 void bootstrap() {
   runZonedGuarded(
