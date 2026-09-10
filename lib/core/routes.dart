516e5a05ad9d27b2c1e43955b17fb2f118161c1f
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-10 02:10:28 -0300

    feat: add LoggedView for user session management and logout functionality

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/view/logged/logged_view.dart';
 import 'package:todo_app/view/login/login_view.dart';
 
 class AppRoutes {
   static const String login = '/login';
+  static const String logged = '/logged';
 
   static final routes = <String, Widget Function(BuildContext)>{
     login: (_) => LoginView(),
+    logged: (_) => LoggedView(),
   };
 }
