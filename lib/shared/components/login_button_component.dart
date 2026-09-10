adcf17382a01631277881b84ea0fa2f8e438fe4f
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-10 02:19:44 -0300

    feat: add form validation to login button component

--- lib/shared/components/login_button_component.dart
+++ lib/shared/components/login_button_component.dart
@@ -6,10 +6,12 @@ import 'package:todo_app/view_models/bloc/login_bloc.dart';
 class LoginButtonComponent extends StatefulWidget {
   const LoginButtonComponent({
     super.key,
+    required this.formKey,
     required this.userController,
     required this.passwordController,
   });
 
+  final GlobalKey<FormState> formKey;
   final TextEditingController userController;
   final TextEditingController passwordController;
 
@@ -28,6 +30,8 @@ class _LoginButtonComponentState extends State<LoginButtonComponent> {
           height: 55,
           child: ElevatedButton(
             onPressed: () async {
+              if (!(widget.formKey.currentState?.validate() ?? false)) return;
+
               context.read<LoginBloc>().add(
                 LoginUserEvent(
                   user: UserLoginModel(
