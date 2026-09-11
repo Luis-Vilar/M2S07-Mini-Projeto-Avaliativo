9c1b4688326ca1caa5dfe2d130c59c6efd762191
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-11 16:48:21 -0300

    refactor: update TodosBloc to use TodoRepositoryInterface injection

--- lib/view_models/bloc/todos_bloc.dart
+++ lib/view_models/bloc/todos_bloc.dart
@@ -2,8 +2,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/core/injection.dart';
 import 'package:todo_app/shared/data/models/todo/todo_model.dart';
 import 'package:todo_app/shared/data/models/user/user_model.dart';
-import 'package:todo_app/shared/data/repositories/todos/todos_repository.dart';
 import 'package:todo_app/shared/data/sources/local/shared_preferences.dart';
+import 'package:todo_app/shared/interfaces/todo_repository.dart';
 import 'package:todo_app/shared/result_pattern.dart';
 
 part 'todos_event.dart';
@@ -11,7 +11,7 @@ part 'todos_state.dart';
 
 class TodosBloc extends Bloc<TodosEvent, TodosState> {
   TodosBloc() : super(TodosInitial()) {
-    final repository = injection.get<TodosRepository>();
+    final repository = injection.get<TodoRepositoryInterface>();
 
     on<TodosSyncEvent>((event, emit) async {
       await _runWithLoading(emit, () async {
