a9e69678e18d4af5e89c42ee9316d948eed98d53
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-11 12:37:07 -0300

    feat: implement light theme and apply it to the main app

--- lib/core/main_app.dart
+++ lib/core/main_app.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/core/routes.dart';
+import 'package:todo_app/core/light_theme.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(routes: AppRoutes.routes, initialRoute: AppRoutes.login);
+    return MaterialApp(
+      routes: AppRoutes.routes,
+      initialRoute: AppRoutes.login,
+      theme: lightTheme,
+    );
   }
 }
