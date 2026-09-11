0b3087ad13f5e4d7c1f7c5a80f8154ab09d1f401
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-11 16:41:03 -0300

    style: rename file

--- lib/shared/implementations/sqflite_crud_todo_implementation.dart
+++ lib/shared/implementations/sqflite_crud_todo_implementation.dart
@@ -1,6 +1,6 @@
 import 'package:todo_app/shared/data/db_helper.dart';
 import 'package:todo_app/shared/data/models/todo/todo_model.dart';
-import 'package:todo_app/shared/interfaces/todo_repository.dart';
+import 'package:todo_app/shared/interfaces/crud_todo_repository.dart';
 
 class SqfliteCrudTodo implements CrudTodoInterface {
   @override
