b7bf2e0ce7348d353709e56caf4503d1bd2ff0f7
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-11 16:47:39 -0300

    refactor: implement TodoRepositoryInterface in TodosRepository

--- lib/shared/data/repositories/todos/todos_repository.dart
+++ lib/shared/data/repositories/todos/todos_repository.dart
@@ -1,27 +1,34 @@
 import 'package:todo_app/core/injection.dart';
 import 'package:todo_app/shared/data/models/todo/todo_model.dart';
+import 'package:todo_app/shared/interfaces/crud_todo_repository.dart';
 import 'package:todo_app/shared/interfaces/todo_repository.dart';
 import 'package:todo_app/shared/interfaces/todo_source.dart';
 import 'package:todo_app/shared/result_pattern.dart';
 
-class TodosRepository {
+class TodosRepository implements TodoRepositoryInterface {
   final _todoRepository = injection.get<CrudTodoInterface>();
   final _todoSource = injection.get<TodoSourceInterface>();
 
+  @override
   Future<void> insertTodos(List<TodoModel> todos) async {
     for (final todo in todos) {
       await _todoRepository.insertTodo(todo);
     }
   }
 
+  @override
   Future<void> insertTodo(TodoModel todo) => _todoRepository.insertTodo(todo);
 
+  @override
   Future<List<TodoModel>> getTodos() => _todoRepository.getTodos();
 
+  @override
   Future<void> updateTodo(TodoModel todo) => _todoRepository.updateTodo(todo);
 
+  @override
   Future<void> deleteTodo(int id) => _todoRepository.deleteTodo(id);
 
+  @override
   Future<Result<List<TodoModel>>> syncTodos(int userId) async {
     final localTodos = await getTodos();
     final hasTodosFromAnotherUser = localTodos.any(
@@ -40,5 +47,6 @@ class TodosRepository {
     return result;
   }
 
+  @override
   Future<void> deleteTodos() => _todoRepository.deleteTodos();
 }
