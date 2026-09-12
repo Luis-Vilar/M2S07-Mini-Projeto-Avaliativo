d383f0376c5d875574a0b4338cd980bbc12dead0
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-12 11:18:10 -0300

    style: rename filename and import path

--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -2,7 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:todo_app/shared/data/repositories/todos/todos_repository.dart';
 import 'package:todo_app/shared/data/sources/external/auth_source.dart';
 import 'package:todo_app/shared/data/sources/external/todo_source.dart';
-import 'package:todo_app/shared/implementations/dio_implementation.dart';
+import 'package:todo_app/shared/implementations/http_client_dio_implementation.dart';
 import 'package:todo_app/shared/implementations/sqflite_crud_todo_implementation.dart';
 import 'package:todo_app/shared/interfaces/auth.dart';
 import 'package:todo_app/shared/interfaces/http_client.dart';
