f6b1385ef0db12b08060c734023ad14794c7e4b3
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-09-12 14:20:16 -0300

    refactor: convert SplashView to StatefulWidget and add animation effects

--- lib/view/splash_screen/splash_view.dart
+++ lib/view/splash_screen/splash_view.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
-  const new({super.key});
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1200),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(
+      begin: 0.85,
+      end: 1.15,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.slowMiddle));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,15 +39,34 @@ class SplashView extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: .center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const CircularProgressIndicator(
-                padding: EdgeInsets.only(bottom: 12),
-                strokeWidth: 6,
-              ),
-              Text(
-                'Carregando...',
-                style: Theme.of(context).textTheme.bodyMedium,
+              ScaleTransition(
+                scale: _animation,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 24),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        'assets/images/logo.png',
+                        width: 180,
+                        height: 180,
+                        errorBuilder: (context, error, stackTrace) {
+                          return const Icon(
+                            Icons.rocket_launch_rounded,
+                            size: 100,
+                            color: Colors.blue,
+                          );
+                        },
+                      ),
+                      Text(
+                        'Carregando...',
+                        style: Theme.of(context).textTheme.bodyLarge
+                            ?.copyWith(fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
